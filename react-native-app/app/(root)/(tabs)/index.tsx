winqus/Restate-React-Native

Chore lint & format code. `npx eslint --fix .`

app/(root)/(tabs)/index.tsx
@@ -1,8 +1,8 @@
 import { Text, View } from "react-native";
-import {Link} from "expo-router";
+import { Link } from "expo-router";
 
 export default function Index() {
-    return (
+  return (
     <View
       style={{
         flex: 1,
@@ -10,11 +10,13 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-        <Text className="font-bold my-10 font-rubik text-3xl">Welcome to Restate</Text>
-        <Link href="/sign-in">Sign In</Link>
-        <Link href="/explore">Explore</Link>
-        <Link href="/profile">Profile</Link>
-        <Link href="/properties/1">Property</Link>
+      <Text className="font-bold my-10 font-rubik text-3xl">
+        Welcome to Restate
+      </Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
     </View>
   );
 }

winqus/KORBIS

react-native-app/app/(root)/(tabs)/index.tsx
@@ -1,8 +1,8 @@
 import { Text, View } from "react-native";
-import {Link} from "expo-router";
+import { Link } from "expo-router";
 
 export default function Index() {
-    return (
+  return (
     <View
       style={{
         flex: 1,
@@ -10,11 +10,13 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-        <Text className="font-bold my-10 font-rubik text-3xl">Welcome to Restate</Text>
-        <Link href="/sign-in">Sign In</Link>
-        <Link href="/explore">Explore</Link>
-        <Link href="/profile">Profile</Link>
-        <Link href="/properties/1">Property</Link>
+      <Text className="font-bold my-10 font-rubik text-3xl">
+        Welcome to Korbis
+      </Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
     </View>
   );
 }